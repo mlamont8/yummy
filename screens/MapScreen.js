398a006661c59855ee3c93b6a5028fbfcb393e2e
mlamont8 <mlamont8@hotmail.com>
2019-06-30 08:41:14 -0400

    added markers to map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Image, Text } from "react-native";
 import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
 import { connect } from "react-redux";
 
 class MapScreen extends React.Component {
   render() {
-    return <MapView style={{ flex: 1 }} />;
+    let region = {
+      latitude: this.props.latitude,
+      longitude: this.props.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    };
+    return (
+      <MapView style={{ flex: 1 }} region={region}>
+        {this.props.list.map(marker => (
+          <Marker
+            key={marker.id}
+            coordinate={{
+              latitude: marker.coordinates.latitude,
+              longitude: marker.coordinates.longitude
+            }}
+            title={marker.name}
+          >
+            <Text>{marker.name}</Text>
+            <Image
+              source={{ uri: marker.image_url }}
+              style={{ width: 20, height: 20, borderRadius: 10 }}
+            />
+          </Marker>
+        ))}
+      </MapView>
+    );
   }
 }
 
@@ -14,4 +41,7 @@ const mapStateToProps = state => ({
   latitude: state.locationReducer.latitude
 });
 
-export default MapScreen;
+export default connect(
+  mapStateToProps,
+  null
+)(MapScreen);
